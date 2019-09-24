62b1246cb384c2337c132fb6ad1b5a4bd9c6a54e
Victor Kody <kody.nakamatsu@gmail.com>
2019-09-23 22:37:05 -0300

    add Spark 1.6 implementation

--- src/main/scala/ReadCSVFile.scala
+++ src/main/scala/ReadCSVFile.scala
@@ -3,25 +3,23 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object ReadCSVFile {
 
+  case class Employee(empno:String, ename:String, designation:String, manager:String, hire_date:String, sal:String , deptno:String)
+
   def main(args : Array[String]): Unit = {
     val sparkConf = new SparkConf().setAppName("CSV Scala Reader").
-      setMaster("local[2]") //set spark configuration
+      setMaster("local[*]") //set spark configuration
 
     val sc = new SparkContext(sparkConf)
     val sqlContext = new SQLContext(sc)
-
     import sqlContext.implicits._
-
-    val filePath = "src\\main\\resources\\daily_conciliations.csv";
-    val file = sc.textFile(filePath)
-
-    val employes = sqlContext.read.format("com.databricks.spark.csv")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .load(filePath)
-
-    val selectedDesignation = employes.
-
-    employes.show()
+    val textRDD = sc.textFile("src\\main\\resources\\emp_data.csv")
+    //println(textRDD.foreach(println)
+    val empRdd = textRDD.map {
+      line =>
+        val col = line.split(",")
+        Employee(col(0), col(1), col(2), col(3), col(4), col(5), col(6))
+    }
+    val empDF = empRdd.toDF()
+    empDF.show()
   }
 }
